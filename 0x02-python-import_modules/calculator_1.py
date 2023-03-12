2b636f2e475d2672224a1befa2d52d22c0ec8699
Jhunyingan <jhunyingan1@gmail.com>
2023-03-11 23:58:05 -0800

    task1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add(x, y):
+    """Returns the sum of x and y."""
+    return x + y
+
+def sub(x, y):
+    """Returns the difference of x and y."""
+    return x - y
+
+def mul(x, y):
+    """Returns the product of x and y."""
+    return x * y
+
+def div(x, y):
+    """Returns the quotient of x and y."""
+    return int(x / y)
+
